4bbd8e94ba5a8c0b180e2c52e7ed1d57400f5fcc
joshuaimanuel1 <joshuadjamiwadu@gmail.com>
2025-11-26 01:59:41 +0700

    Disable internal test runner for Vercel

--- server.js
+++ server.js
@@ -45,16 +45,16 @@ module.exports = app;
 if (require.main === module) {
   const listener = app.listen(process.env.PORT || 3000, function () {
     console.log("Your app is listening on port " + listener.address().port);
-    if (process.env.NODE_ENV === "test") {
-      console.log("Running Tests...");
-      setTimeout(function () {
-        try {
-          runner.run();
-        } catch (e) {
-          console.log("Tests are not valid:");
-          console.error(e);
-        }
-      }, 1500);
-    }
+    //if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch (e) {
+        console.log("Tests are not valid:");
+        console.error(e);
+      }
+    }, 1500);
+    //}
   });
 }
